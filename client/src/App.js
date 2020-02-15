2a0d7fb961c88dde437b8981c1aaec75474141bf
Rado Jedro <radoslaw.jedro@gmail.com>
2020-02-15 23:18:34 +0000

    Add functionality to client app

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,116 @@
 import React from 'react';
+import io from 'socket.io-client';
+import uuidv1 from 'uuid';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tasks: [],
+      taskName: {}
+    };
+
+    this.submitForm = this.submitForm.bind(this);
+    this.changeValue = this.changeValue.bind(this);
+  }
+
+  componentDidMount() {
+    this.socket = io('http://localhost:8000');
+
+    this.socket.on('addTask', newTask => {
+      this.addTask(newTask);
+    });
+    this.socket.on('removeTask', (index, task) => {
+      this.removeTask(index, task);
+    });
+    this.socket.on('updateData', tasks => {
+      this.updateData(tasks);
+    });
+  }
+
+  updateData(tasks) {
+    this.setState({ tasks: tasks });
+  }
+
+  submitForm(event) {
+    const { taskName } = this.state;
+    event.preventDefault();
+    this.addTask(taskName);
+    this.socket.emit('addTask', taskName);
+  }
+
+  addTask(newTask) {
+    const { tasks } = this.state;
+    if (!tasks.find(task => task.id === newTask.id)) {
+      tasks.push(newTask);
+      this.setState(tasks);
+    }
+  }
+
+  removeTask(e, task) {
+    const { tasks } = this.state;
+    const index = tasks.indexOf(task);
+
+    if (tasks.find(taskToRemove => taskToRemove.id === task.id)) {
+      this.setState(tasks.splice(index, 1));
+      this.socket.emit('removeTask', index, task);
+    }
+  }
+
+  changeValue(event) {
+    this.setState({
+      taskName: {
+        name: event.target.value,
+        id: uuidv1()
+      }
+    });
+  }
 
   render() {
+    const { tasks, taskName } = this.state;
     return (
-      <div className="App">
-    
+      <div className='App'>
         <header>
           <h1>ToDoList.app</h1>
         </header>
-    
-        <section className="tasks-section" id="tasks-section">
+
+        <section className='tasks-section' id='tasks-section'>
           <h2>Tasks</h2>
-    
-          <ul className="tasks-section__list" id="tasks-list">
-            <li class="task">Shopping <button class="btn btn--red">Remove</button></li>
-            <li class="task">Go out with a dog <button class="btn btn--red">Remove</button></li>
+
+          <ul className='tasks-section__list' id='tasks-list'>
+            {tasks.map(task => (
+              <li key={task.name} className='task'>
+                {task.name}
+                {''}
+                <button
+                  onClick={e => this.removeTask(e, task)}
+                  className='btn btn--red'
+                >
+                  Remove
+                </button>
+              </li>
+            ))}
           </ul>
-    
-          <form id="add-task-form">
-            <input className="text-input" autocomplete="off" type="text" placeholder="Type your description" id="task-name" />
-            <button className="btn" type="submit">Add</button>
+
+          <form id='add-task-form' onSubmit={this.submitForm}>
+            <input
+              className='text-input'
+              autoComplete='off'
+              type='text'
+              placeholder='Type your description'
+              id='task-name'
+              value={taskName.name}
+              onChange={this.changeValue}
+            />
+            <button className='btn' type='submit'>
+              Add
+            </button>
           </form>
-    
         </section>
       </div>
     );
-  };
-
-};
+  }
+}
 
 export default App;
