250390da057f962f743c9adaa2aa04e0bf9dfad1
Rado Jedro <radoslaw.jedro@gmail.com>
2020-02-15 20:51:27 +0000

    Add socket.io

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
+const socket = require('socket.io'); 
+
+const tasks = [];
+
 const server = app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running...');
+  console.log("Server is running...");
 });
 
 app.use((req, res) => {
-  res.status(404).send({ message: 'Not found...' });
+  res.status(404).send({ message: "Not found..." });
+});
+
+const io = socket(server);
+
+io.on("connection", socket => {
+  console.log(socket.io, "connected");
+
+  socket.on("disconnect", socket => {
+    console.log(socket.io, "disconnected");
+  });
 });
